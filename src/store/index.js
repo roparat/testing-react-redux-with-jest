e36cbef19140b4033bb858882a7e5b623dd0982c
Roparat Sukapirom <roparat@gmail.com>
2017-03-20 16:41:51 +0700

    add redux debug

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ const createStore = (initialState) => {
   const store = createReduxStore(
     rootReducer,
     initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   )
   return store
 }
