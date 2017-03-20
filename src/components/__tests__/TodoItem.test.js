dc0e219727869fa5afea6fec1c91b620284d8138
Roparat Sukapirom <roparat@gmail.com>
2017-03-20 11:50:30 +0700

    using enzyme

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -6,6 +6,13 @@ describe('TodoItem', () => {
   it('render correct structure', () => {
     const component = shallow(<TodoItem text={'Hello'} />)
     
-    expect(component.is('li')).toBe(true);
+    expect(component.is('li')).toBe(true)
+  })
+
+  it('has complete class if receiving complete prop', () => {
+    const component = shallow(<TodoItem text="Hello" complete />);
+
+    expect(component.hasClass('complete')).toBe(true)
+    expect(component.text()).toBe('Hello')
   })
 })
