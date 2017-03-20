2229c81542d87f98570c359c90cf2b34e103c1ed
Roparat Sukapirom <roparat@gmail.com>
2017-03-20 14:28:59 +0700

    test one more reducers

--- src/components/reducers/__tests__/todosReducers.test.js
+++ src/components/reducers/__tests__/todosReducers.test.js
@@ -1,10 +1,11 @@
 import todosReducer from '../todoReducer'
+import { ADD_TODO } from '../../../actions/types'
 
 describe('todoReducer', () => {
   it('returns correct initial state', () => {
     expect(todosReducer(undefined, {})).toEqual([])
   })
-  
+
   it('returns current state if no action matched', () => {
     const curState = [
       { id: 1, text: 'Feed cat', complete: false },
@@ -13,5 +14,23 @@ describe('todoReducer', () => {
 
     expect(nextState).toBe(curState) // pointer
     expect(nextState).toEqual(curState) // value
+  })
+
+  it('adds todos when receiving ADD_TODO action', () => {
+    const curState = [
+      { id: 1, text: 'Feed cat', complete: false },
+    ]
+    const action = {
+      type: ADD_TODO,
+      payload: { text: 'Sleep' },
+    }
+    const nextState = todosReducer(curState, action)
+    const expectedState =  [
+      { id: 1, text: 'Feed cat', complete: false },
+      { id: 2, text: 'Sleep', complete: false },
+    ]
+
+    expect(nextState).toEqual(expectedState)
+    expect(nextState).not.toBe(curState) // check for immutable
   })
 })
--- src/components/reducers/todoReducer.js
+++ src/components/reducers/todoReducer.js
@@ -1,7 +1,18 @@
+import { ADD_TODO } from '../../actions/types'
+
 const todoReducerInitialState = []
 
 export const todoReducer = (state = todoReducerInitialState, action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const id = 1 + state.reduce((max, cur) => Math.max(max, cur.id), 0)
+      const newTodo = {
+        id,
+        text: action.payload.text,
+        complete: false,
+      }
+      return ([...state, newTodo])
+    }
     default:
       return state
   }
