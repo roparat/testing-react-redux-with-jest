cbe63cda0d2afb48c631b6b2380b1ee61585637f
Roparat Sukapirom <roparat@gmail.com>
2017-03-20 10:26:54 +0700

    add storybook

---
+++ stories/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+  ))
+  .add('with some emoji', () => (
+  ));
